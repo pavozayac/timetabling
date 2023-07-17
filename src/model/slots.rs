0f7659744377581b306da84805cacd7b0f2159a0
Paweł Zając <pavlzayac@gmail.com>
2023-07-17 17:39:42 +0200

    Add Outline

--- src/model/slots.rs
+++ src/model/slots.rs
@@ -14,3 +14,27 @@ impl Slot {
         self.end.signed_duration_since(self.start)
     }
 }
+
+pub struct Outline {
+    pub slots: Vec<Slot>,
+}
+
+impl Outline {
+    fn new() -> Outline {
+        Outline { slots: vec![] }
+    }
+
+    fn add_slot(&mut self, slot: Slot) {
+        self.slots.push(slot);
+    }
+
+    fn slots(&self) -> &Vec<Slot> {
+        &self.slots
+    }
+}
+
+impl From<Vec<Slot>> for Outline {
+    fn from(value: Vec<Slot>) -> Self {
+        Outline { slots: value }
+    }
+}
