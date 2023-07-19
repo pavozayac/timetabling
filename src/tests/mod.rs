0bcd0b157eeb92cf69f276cd4ddb539fa5f1a7d5
Paweł Zając <pavlzayac@gmail.com>
2023-07-19 14:25:17 +0200

    Add utils tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,1 +1,2 @@
 pub mod model_tests;
+pub mod utils_tests;
---
+++ src/tests/utils_tests.rs
@@ -0,0 +1,61 @@
+use crate::utils::has_unique_items;
+
+#[test]
+pub fn returns_true_given_unique_items() {
+    let slice = &[1, 2, 3, 4, 5, 6];
+
+    assert_eq!(true, has_unique_items(slice));
+}
+
+#[test]
+pub fn returns_false_given_non_unique_items() {
+    let slice = &[1, 2, 3, 4, 1, 6];
+
+    assert_eq!(false, has_unique_items(slice));
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct TestStruct<'a> {
+    string: &'a str,
+    number: i64,
+}
+
+#[test]
+pub fn returns_true_given_unique_structs() {
+    let slice = &[
+        TestStruct {
+            string: "hello",
+            number: 1,
+        },
+        TestStruct {
+            string: "hello",
+            number: 2,
+        },
+        TestStruct {
+            string: "goodbye",
+            number: 1000000,
+        },
+    ];
+
+    assert_eq!(true, has_unique_items(slice));
+}
+
+#[test]
+pub fn returns_false_given_unique_structs() {
+    let slice = &[
+        TestStruct {
+            string: "hello",
+            number: 1,
+        },
+        TestStruct {
+            string: "hello",
+            number: 1,
+        },
+        TestStruct {
+            string: "goodbye",
+            number: 1000000,
+        },
+    ];
+
+    assert_eq!(false, has_unique_items(slice));
+}
