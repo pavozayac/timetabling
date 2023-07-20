f464d117285a45bc1bf84b218ac3a010eff5459d
Paweł Zając <pavlzayac@gmail.com>
2023-07-20 13:13:10 +0200

    Fix tests which use Outline

--- src/tests/model/events_tests.rs
+++ src/tests/model/events_tests.rs
@@ -6,54 +6,58 @@ use crate::model::{
 
 #[test]
 pub fn errors_with_incorrect_constraints() {
+    let mut outline = Outline::new();
+    outline.extend_from_slice(&[Slot::new(1)]).unwrap();
+
     let event = EventBuilder::new(1)
-        .resource_constraints(vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)]))])
+        .resource_constraints(vec![Resource::new(1, 1, outline.clone())])
         .build();
 
-    let res = event.assign(
-        Slot::new(1),
-        vec![Resource::new(2, 1, Outline::from(vec![Slot::new(2)]))],
-    );
+    let res = event.assign(Slot::new(1), vec![Resource::new(2, 1, outline)]);
 
     assert_eq!(res, Err(()));
 }
 
 #[test]
 pub fn assigns_with_correct_constraints() {
+    let mut outline = Outline::new();
+    outline.extend_from_slice(&[Slot::new(1)]).unwrap();
+
     let event = EventBuilder::new(1)
-        .resource_constraints(vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)]))])
+        .resource_constraints(vec![Resource::new(1, 1, outline.clone())])
         .build();
 
-    let res = event.clone().assign(
-        Slot::new(1),
-        vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)]))],
-    );
+    let res = event
+        .clone()
+        .assign(Slot::new(1), vec![Resource::new(1, 1, outline.clone())]);
 
     assert_eq!(
         res,
         Ok(EventInstance {
             event: event,
             assigned_slot: Slot::new(1),
-            assigned_resources: vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)],))],
+            assigned_resources: vec![Resource::new(1, 1, outline)],
         })
     );
 }
 
 #[test]
 pub fn assigns_with_no_constraints() {
+    let mut outline = Outline::new();
+    outline.extend_from_slice(&[Slot::new(1)]).unwrap();
+
     let event = EventBuilder::new(1).build();
 
-    let res = event.clone().assign(
-        Slot::new(1),
-        vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)]))],
-    );
+    let res = event
+        .clone()
+        .assign(Slot::new(1), vec![Resource::new(1, 1, outline.clone())]);
 
     assert_eq!(
         res,
         Ok(EventInstance {
             event: event,
             assigned_slot: Slot::new(1),
-            assigned_resources: vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)],))],
+            assigned_resources: vec![Resource::new(1, 1, outline)],
         })
     );
 }
--- src/tests/model/schedule_tests.rs
+++ src/tests/model/schedule_tests.rs
@@ -9,18 +9,15 @@ pub fn instantiates_with_no_conflicts() {
     let event1 = EventBuilder::new(1).build();
     let event2 = EventBuilder::new(2).build();
 
+    let mut outline = Outline::new();
+    outline.extend_from_slice(&[Slot::new(1)]).unwrap();
+
     let ei1 = event1
-        .assign(
-            Slot::new(1),
-            vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)]))],
-        )
+        .assign(Slot::new(1), vec![Resource::new(1, 1, outline.clone())])
         .unwrap();
 
     let ei2 = event2
-        .assign(
-            Slot::new(1),
-            vec![Resource::new(2, 1, Outline::from(vec![Slot::new(1)]))],
-        )
+        .assign(Slot::new(1), vec![Resource::new(2, 1, outline)])
         .unwrap();
 
     assert!(matches!(Schedule::new(vec![ei1, ei2]), Ok(_)));
@@ -31,18 +28,15 @@ pub fn fails_with_conflicts() {
     let event1 = EventBuilder::new(1).build();
     let event2 = EventBuilder::new(2).build();
 
+    let mut outline = Outline::new();
+    outline.extend_from_slice(&[Slot::new(1)]).unwrap();
+
     let ei1 = event1
-        .assign(
-            Slot::new(1),
-            vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)]))],
-        )
+        .assign(Slot::new(1), vec![Resource::new(1, 1, outline.clone())])
         .unwrap();
 
     let ei2 = event2
-        .assign(
-            Slot::new(1),
-            vec![Resource::new(1, 1, Outline::from(vec![Slot::new(1)]))],
-        )
+        .assign(Slot::new(1), vec![Resource::new(1, 1, outline)])
         .unwrap();
 
     assert!(matches!(Schedule::new(vec![ei1, ei2]), Err(_)));
