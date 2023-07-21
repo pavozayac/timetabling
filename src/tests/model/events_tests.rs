571476151a069a2c4c8fe3ed02fb375ad6f63b73
Paweł Zając <pavlzayac@gmail.com>
2023-07-21 19:46:14 +0200

    Add time_constraints and fixed_slot tests for Event

--- src/tests/model/events_tests.rs
+++ src/tests/model/events_tests.rs
@@ -5,7 +5,7 @@ use crate::model::{
 };
 
 #[test]
-pub fn errors_with_incorrect_constraints() {
+pub fn errors_with_incorrect_resource_constraints() {
     let mut outline = Outline::new();
     outline.extend_from_slice(&[Slot::new(1)]).unwrap();
 
@@ -19,7 +19,7 @@ pub fn errors_with_incorrect_constraints() {
 }
 
 #[test]
-pub fn assigns_with_correct_constraints() {
+pub fn assigns_with_correct_resource_constraints() {
     let mut outline = Outline::new();
     outline.extend_from_slice(&[Slot::new(1)]).unwrap();
 
@@ -42,7 +42,7 @@ pub fn assigns_with_correct_constraints() {
 }
 
 #[test]
-pub fn assigns_with_no_constraints() {
+pub fn assigns_with_no_resource_constraints() {
     let mut outline = Outline::new();
     outline.extend_from_slice(&[Slot::new(1)]).unwrap();
 
@@ -60,4 +60,46 @@ pub fn assigns_with_no_constraints() {
             assigned_resources: vec![Resource::new(1, 1, outline)],
         })
     );
+}
+
+#[test]
+pub fn assigns_with_correct_fixed_slot() {
+    let event = EventBuilder::new(1).fixed_slot(Slot::new(1)).build();
+
+    let res = event.clone().assign(Slot::new(1), vec![]);
+
+    assert!(matches!(res, Ok(_)));
+}
+
+#[test]
+pub fn errors_with_wrong_fixed_slot() {
+    let event = EventBuilder::new(1).fixed_slot(Slot::new(2)).build();
+
+    let res = event.clone().assign(Slot::new(1), vec![]);
+
+    assert!(matches!(res, Err(_)));
+}
+
+#[test]
+pub fn assigns_within_time_constraints() {
+    let mut outline = Outline::new();
+    outline.extend_from_slice(&[Slot::new(1)]).unwrap();
+
+    let event = EventBuilder::new(1).time_constraints(outline).build();
+
+    let res = event.clone().assign(Slot::new(1), vec![]);
+
+    assert!(matches!(res, Ok(_)));
+}
+
+#[test]
+pub fn errors_outside_time_constraints() {
+    let mut outline = Outline::new();
+    outline.extend_from_slice(&[Slot::new(2)]).unwrap();
+
+    let event = EventBuilder::new(1).time_constraints(outline).build();
+
+    let res = event.clone().assign(Slot::new(1), vec![]);
+
+    assert!(matches!(res, Err(_)));
 }
