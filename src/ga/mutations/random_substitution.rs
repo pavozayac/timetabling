3b4424ca18aa4dbeccfb9972bad7d3e124f203ac
pavozayac <pavlzayac@gmail.com>
2023-09-03 16:18:17 +0200

    Improve mutations on resources

--- src/ga/mutations/random_substitution.rs
+++ src/ga/mutations/random_substitution.rs
@@ -44,23 +44,35 @@ impl<'a, R: Rng> Mutation for RandomSubstitutionMutation<'a, R> {
 
             chromosome.set_slot(event_choice, slot_substitute);
 
-            let resource_substitute: ResourceIDPair = self
-                .problem_domain
-                .resources
-                .choose(&mut self.rng)
-                .unwrap()
-                .into();
-
             let resources: &mut Vec<ResourceIDPair> = chromosome.get_resources_mut(EventID(
                 self.rng.gen_range(0..self.problem_domain.events.len()),
             ));
 
-            let extend: bool = self.rng.gen_bool(0.5);
-
-            if extend {
-                resources.push(resource_substitute);
+            // 50% chance to either extend the list of resources assigned to the event,
+            // or to substitute for another resource instead.
+            let chance = if resources.len() > 0 {
+                1.0 / (resources.len() + 1)
             } else {
-                *resources.choose_mut(&mut self.rng).unwrap() = resource_substitute;
+                0.0
+            };
+
+            let delete: bool = self.rng.gen_bool(chance);
+
+            if delete {
+                resources.swap_remove(self.rng.gen_range(0..resources.len()));
+            } else {
+                let resource_substitute: ResourceIDPair = self
+                    .problem_domain
+                    .resources
+                    .choose(&mut self.rng)
+                    .unwrap()
+                    .into();
+
+                if resources.is_empty() || self.rng.gen_bool(chance) {
+                    resources.push(resource_substitute);
+                } else {
+                    *resources.choose_mut(&mut self.rng).unwrap() = resource_substitute;
+                }
             }
 
             counter -= 1.0;
